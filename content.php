5916637972e700b8cc649c927bacec6e466d3697
unknown <you@example.com>
2021-01-06 20:16:44 +0000

    first commit

---
+++ content.php
@@ -0,0 +1,25 @@
+
+<?php include("includes/header.ph"); ?>
+
+		<table id="structure">
+			<tr>
+				<td id="navigation">
+					&nbsp;
+				</td>
+				<td>
+					<h2>Staff Menu</h2>
+					<p>Welcome to the staff area.</p>
+					<ul>
+						<li><a href="content.php">Manage website content</a></li>
+						<li><a href="new_user.php">Add Staff User</a></li>
+						<li><a href="logout.php">Logout</a></li>
+					</ul>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<img src="images/font">
+	<div id="footer">copyright 2020, Widget Corp</div>
+
+</body>
+</html>
